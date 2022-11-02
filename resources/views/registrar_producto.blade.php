f8157df95c158a34423510007ad6013752e6fe6a
Gustavo Ovalle <gustavo.ovalle.zambrano@gmail.com>
2022-11-02 00:10:18 -0300

    02/11/22 - formulario de crear producto hecho (vista)

--- resources/views/registrar_producto.blade.php
+++ resources/views/registrar_producto.blade.php
@@ -3,12 +3,16 @@
     Producto
 @endsection
 @section('content')
-<header class=" p-5 flex justify-center bg-sky-500">
-    <h1>Producto</h1>
+<header class=" p-5 flex justify-center">
+    <h1 class="text-2xl">Producto</h1>
 </header>
-<main class="bg-red-500 grid justify-center grid-cols-2">
-    <h1>Imagen</h1>
-    <div class="bg-white rounded-lg p-2">
+<main class="grid justify-center grid-cols-2 p-5">
+    <div class="p-5 flex items-center">
+        <form action="/imagen" method="POSt" enctype="multipart/form-data" id="dropzone" class="bg-white drop-shadow-lg dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+
+        </form>
+    </div>
+    <div class="bg-white rounded-lg p-5 drop-shadow-lg">
         <form action="#">
             <div class="flex flex-col">
                 <div>
@@ -28,7 +32,7 @@
                 </div>
                 <input class="p-2 border-4 border-l-orange-500 @error('price') border-l-red-700 @enderror" type="number" name="price" id="price" placeholder="$1000" value="{{old('price')}}">
             </div>
-            <div>
+            <div class="flex flex-col">
                 <div>
                     <label for="brand">Marca</label>
                     @error('brand')
@@ -37,11 +41,28 @@
                 </div>
                 <input class="p-2 border-4 border-l-orange-500 @error('brand') border-l-red-700 @enderror" type="text" name="brand" id="brand" placeholder="Marca del producto" value="{{old('brand')}}">
             </div>
-            <div>
-                <label for="stock">stock</label>
+            <div class="flex flex-col">
+                <div>
+                    <label for="stock">stock</label>
+                    @error('stock')
+                    <span class="text-red-600"><small>*{{$message}}</small></span>
+                    @enderror
+                </div>
+                <input class="p-2 border-4 border-l-orange-500 @error('stock') border-l-red-700 @enderror" type="number" name="stock" id="stock" placeholder="999" value="{{old('stock')}}">
             </div>
-            <div>
-                <label for="code">Codigo</label>
+            <div class="flex flex-col">
+                <div>
+                    <label for="code">Codigo</label>
+                    @error('code')
+                    <span class="text-red-600"><small>*{{$message}}</small></span>
+                    @enderror
+                </div>
+                <input class="p-2 border-4 border-l-orange-500 @error('code') border-l-red-700 @enderror" type="number" name="code" id="code" placeholder="4589612354" value="{{old('code')}}">
+            </div>
+            <div class="pt-5 flex gap-3">
+                <button class="bg-orange-500 w-full rounded-xl hover:bg-orange-600 p-2 text-white">Registrar</button>
+                <button class="bg-red-500 w-full rounded-xl hover:bg-red-600 p-2 text-white">cancelar</button>
+                <button class="bg-green-500 w-full rounded-xl hover:bg-green-600 p-2 text-white">Registrar y siguiente</button>
             </div>
         </form>
         
